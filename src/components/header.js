71fe4c3ceb6f0f52123d7e5cfa619eae02304b6a
demkantor <demkantor@gmail.com>
2020-06-15 17:38:40 -0500

    inital nav logic added

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,53 @@
-import { Link } from "gatsby"
+import React, { Component } from "react";
 import PropTypes from "prop-types"
-import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div class="logo">
-        <img src="images/david-kantor-icon.png" alt="" />
-    </div>
-    <button class="nav-toggle" aria-label="toggle navigation">
-        <span class="hamburger"></span>
-    </button>
-    <nav class="nav">
-        <ul class="nav__list">
-            <li class="nav__item"><a href="#home" class="nav__link">Home</a></li>
-            <li class="nav__item"><a href="#services" class="nav__link">My Services</a></li>
-            <li class="nav__item"><a href="#about" class="nav__link">About me</a></li>
-            <li class="nav__item"><a href="#work" class="nav__link">My Work</a></li>
-        </ul>
-    </nav>
-  </header>
-)
+class Header extends Component {
+
+  closeNav = () => {
+    document.body.classList.remove('nav-open');
+  };
+
+  openNav = () => {
+    document.body.classList.toggle('nav-open');
+  };
+
+  render() {
+    return (
+      <header>
+        <div className="logo">
+            <img src="images/david-kantor-icon.png" alt="" />
+        </div>
+        <button className="nav-toggle" aria-label="toggle navigation" onClick={this.openNav}>
+            <span className="hamburger"></span>
+        </button>
+        <nav className="nav">
+            <ul className="nav__list">
+                <li className="nav__item" onClick={this.closeNav}>
+                  <a href="#home" className="nav__link">
+                    Home
+                  </a>
+                </li>
+                <li className="nav__item" onClick={this.closeNav}>
+                  <a href="#services" className="nav__link">
+                    My Services
+                  </a>
+                </li>
+                <li className="nav__item" onClick={this.closeNav}>
+                  <a href="#about" className="nav__link">
+                    About me
+                  </a>
+                </li>
+                <li className="nav__item" onClick={this.closeNav}>
+                  <a href="#work" className="nav__link">
+                    My Work
+                  </a>
+                </li>
+            </ul>
+        </nav>
+      </header>
+    )
+  }
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -29,4 +57,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+export default Header;
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Intro = () => (
-    <section class="intro" id="home">
-        <h1 class="section__title section__title--intro">
+    <section className="intro" id="home">
+        <h1 className="section__title section__title--intro">
             Hi, I am <strong>David Kantor</strong>
         </h1>
-        <p class="section__subtitle section__subtitle--intro">full-stack developer</p>
-        <img src="images/david-25.jpg" alt="a picture of me smiling" class="intro__img" />
+        <p className="section__subtitle section__subtitle--intro">full-stack developer</p>
+        <img src="images/david-25.jpg" alt="Me smiling" className="intro__img" />
     </section>
 );
 
