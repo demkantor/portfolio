f64158b58dc7873b9ee9333041a9a76769d292b5
demkantor <demkantor@gmail.com>
2020-06-15 16:52:43 -0500

    getting images in place

---
+++ src/components/footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="footer">
+        <a href="mailto:davidedwardkantor@gmail.com" className="footer__link">davidedwardkantor@gmail.com</a>
+        <ul className="social-list">
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://github.com/demkantor">
+                    <i className="fab fa-github"></i>
+                </a>
+            </li>
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://codepen.io/demkantor">
+                    <i className="fab fa-codepen"></i>
+                </a>
+            </li>
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://forum.xda-developers.com/member.php?u=4334383">
+                    <i className="fab fa-android"></i>
+                </a>
+            </li>
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://www.demkantor.com/">
+                    <i className="fas fa-rss"></i>
+                </a>
+            </li>
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://linkedin.com/in/david-edward-kantor">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </li>
+        </ul>
+    </footer>
+);
+
+
+export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,21 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div class="logo">
+        <img src="images/david-kantor-icon.png" alt="" />
     </div>
+    <button class="nav-toggle" aria-label="toggle navigation">
+        <span class="hamburger"></span>
+    </button>
+    <nav class="nav">
+        <ul class="nav__list">
+            <li class="nav__item"><a href="#home" class="nav__link">Home</a></li>
+            <li class="nav__item"><a href="#services" class="nav__link">My Services</a></li>
+            <li class="nav__item"><a href="#about" class="nav__link">About me</a></li>
+            <li class="nav__item"><a href="#work" class="nav__link">My Work</a></li>
+        </ul>
+    </nav>
   </header>
 )
 
